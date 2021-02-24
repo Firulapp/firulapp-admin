576e8c15e5b6199f57adefd6f110d322d3a315d2
Javier Heisecke <javierheiseckee@gmail.com>
2021-02-23 21:50:34 -0300

    Agrega formulario para creacion y edicion de razas

---
+++ src/resources/razas/Create.vue
@@ -0,0 +1,11 @@
+<template>
+  <va-create-layout :title="title">
+    <razas-form :item="item"></razas-form>
+  </va-create-layout>
+</template>
+
+<script>
+export default {
+  props: ["id", "title", "item"]
+};
+</script>
