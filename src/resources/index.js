3269ace4b6aa7a6ac7ce2bdfd1291d42d5f0f72e
Javier Heisecke <javierheiseckee@gmail.com>
2021-03-06 14:13:24 -0300

    Ajusta endpoints a api

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,30 +1,36 @@
 export default [
   {
+    api: "/param/service/type",
     name: "servicios",
     icon: "mdi-briefcase",
     label: "name"
   },
   {
+    api: "/param/breed",
     name: "razas",
     icon: "mdi-cat",
     label: "name"
   },
   {
+    api: "/param/species",
     name: "especies",
     icon: "mdi-dog",
     label: "name"
   },
   {
+    api: "/param/petcare",
     name: "cuidados",
     icon: "mdi-doctor",
     label: "name"
   },
   {
+    api: "/param/organizations",
     name: "organizaciones",
     icon: "mdi-email",
     label: "name"
   },
   {
+    api: "/param/help",
     name: "ayudas",
     icon: "mdi-message-video",
     label: "name"
