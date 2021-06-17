810546178ee905b455e4f84189ed69b7d04b2fd6
jazvillagra <jazvillagra.jv@gmail.com>
2021-06-17 16:55:27 -0400

    Agrega configuraciones para deploy a github pages

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,8 @@ module.exports = {
       enableInSFC: "false"
     }
   },
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/firulapp-admin'
+  : '/',
   transpileDependencies: ["vuetify"]
 };
